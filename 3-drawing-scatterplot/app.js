0429a39a4302a7f6f2bdd837be74c27b21dcde79
Daniel Olaya <daniel.olayam@gmail.com>
2021-06-15 15:36:39 -0700

    scatterplot axis

--- 3-drawing-scatterplot/app.js
+++ 3-drawing-scatterplot/app.js
@@ -43,7 +43,7 @@ async function draw() {
 	// Scales
 	const yScale = d3.scaleLinear()
 		.domain(d3.extent(dataset, yAccessor))
-		.rangeRound([0, dimensions.ctrHeight])
+		.rangeRound([dimensions.ctrHeight, 0])
 		.nice()
 		.clamp(true)
 
@@ -55,10 +55,39 @@ async function draw() {
 		.attr('cy', d => yScale(yAccessor(d)))
 		.attr('r', 5)
 		.attr('fill', 'red')
+		.attr('data-temp', yAccessor)
 
+	//Axes
+	const xAxis = d3.axisBottom(xScale)
+		.ticks(5)
+		.tickFormat((d) => d * 100 + '%'  )
+		//.tickValues([0.4, 0.5, 0.8])
+
+	const xAxisGroup = ctr.append('g')
+		.call(xAxis)
+		.style('transform', `translateY(${dimensions.ctrHeight}px)`)
+		.classed('axis', true)
+
+	xAxisGroup.append('text')
+		.attr('x', dimensions.ctrWidth / 2)
+		.attr('y', dimensions.margin.bottom - 10)
+		.attr('fill', 'black')
+		.text('Humidity')
+
+
+	const yAxis = d3.axisLeft(yScale)
+
+	const yAxisGroup = ctr.append('g')
+		.call(yAxis)
+		.classed('axis', true)
+
+	yAxisGroup.append('text')
+		.attr('x', -dimensions.ctrHeight / 2)
+		.attr('y', -dimensions.margin.left + 15)
+		.attr('fill', 'black')
+		.html('Temperature &deg; F')
+		.style('transform', 'rotate(270deg)')
+		.style('text-anchor', 'middle')
 }
-
-
-
 
 draw()
